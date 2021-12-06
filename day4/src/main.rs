f2c6df3f1edc3b54fdd7a46148ca9e1ec29bef2b
Dario Nesi <dario.nesi@mail.polimi.it>
2021-12-06 22:28:25 +0000

    day 4 - part 2 solution implemented

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::{HashMap, HashSet},
-    iter,
-};
+use std::collections::{HashMap, HashSet};
 
 fn main() -> std::result::Result<(), std::io::Error> {
     let lines = std::fs::read_to_string("input.txt")?;
@@ -16,15 +13,11 @@ fn main() -> std::result::Result<(), std::io::Error> {
 struct BingoGame {
     drawn_numbers: Vec<u32>,
     boards: Vec<BingoBoard>,
-
-    // auxiliary struct for quick lookup
-    number_to_board_indices: HashMap<u32, Vec<usize>>,
 }
 
 #[derive(Clone, Debug)]
 struct BingoBoard {
     drawn_numbers: HashSet<u32>,
-    board: Vec<Vec<u32>>,
 
     // auxiliary struct for quick lookup
     number_to_coordinates: HashMap<u32, (usize, usize)>,
@@ -46,20 +39,9 @@ fn parse_input(lines: Vec<&str>) -> BingoGame {
         .chunks(5)
         .map(|l| parse_board(l))
         .collect();
-    let mut number_to_board_indices = HashMap::<u32, Vec<usize>>::new();
-    for (index, board) in boards.iter().enumerate() {
-        for &number in board.number_to_coordinates.keys() {
-            number_to_board_indices
-                .entry(number)
-                .or_default()
-                .push(index);
-        }
-    }
-
     BingoGame {
         drawn_numbers: drawn_numbers,
         boards: boards,
-        number_to_board_indices: number_to_board_indices,
     }
 }
 
@@ -93,23 +75,6 @@ fn test_parse_given_example_input() {
     // THEN
     assert!(bingo_game.drawn_numbers.len() == 27);
     assert!(bingo_game.boards.len() == 3);
-    assert!(bingo_game.number_to_board_indices.len() == 27);
-    assert!(bingo_game.number_to_board_indices.contains_key(&16));
-    assert!(bingo_game
-        .number_to_board_indices
-        .get(&16)
-        .unwrap()
-        .contains(&0));
-    assert!(bingo_game
-        .number_to_board_indices
-        .get(&16)
-        .unwrap()
-        .contains(&1));
-    assert!(bingo_game
-        .number_to_board_indices
-        .get(&16)
-        .unwrap()
-        .contains(&2));
 }
 
 fn parse_board(lines: &[&str]) -> BingoBoard {
@@ -129,7 +94,6 @@ fn parse_board(lines: &[&str]) -> BingoBoard {
     }
     BingoBoard {
         drawn_numbers: HashSet::new(),
-        board: rows,
         number_to_coordinates: number_to_coordinates,
         drawn_per_column: [0u8; 5],
         drawn_per_row: [0u8; 5],
@@ -165,7 +129,39 @@ fn compute_part1(original_bingo_game: &BingoGame) -> u32 {
     0
 }
 
-fn compute_part2(bingo_game: &BingoGame) -> u32 {
+fn compute_part2(original_bingo_game: &BingoGame) -> u32 {
+    let mut bingo_game: BingoGame = original_bingo_game.clone();
+    // draw number
+    for draw in original_bingo_game.drawn_numbers.iter() {
+        // update boards
+        for board in bingo_game.boards.iter_mut() {
+            if let Some(&(row, col)) = board.number_to_coordinates.get(draw) {
+                board.drawn_numbers.insert(*draw);
+                board.drawn_per_row[row] |= 1 << col;
+                board.drawn_per_column[col] |= 1 << row;
+            }
+        }
+
+        // if there is only one board, it must be the last one that can win
+        if bingo_game.boards.len() == 1 {
+            // when a board wins, compute score: all non drawn numbers added up
+            let sum_numbers_not_drawn: u32 = bingo_game.boards[0]
+                .number_to_coordinates
+                .keys()
+                .filter(|n| !bingo_game.boards[0].drawn_numbers.contains(n))
+                .sum();
+            return sum_numbers_not_drawn * draw;
+        }
+
+        // otherwise filter out the winning ones until only one remains
+        bingo_game.boards = bingo_game
+            .boards
+            .into_iter()
+            .filter(|board| {
+                !board.drawn_per_column.contains(&31) && !board.drawn_per_row.contains(&31)
+            })
+            .collect();
+    }
     0
 }
 
@@ -184,19 +180,10 @@ fn test_parse_given_example_board() {
     let board = parse_board(&lines[0..5]);
 
     // THEN
-    assert!(board.board[0][0] == 22);
     assert!(board.number_to_coordinates[&22] == (0, 0));
-
-    assert!(board.board[1][1] == 2);
     assert!(board.number_to_coordinates[&2] == (1, 1));
-
-    assert!(board.board[4][4] == 19);
     assert!(board.number_to_coordinates[&19] == (4, 4));
-
-    assert!(board.board[4][0] == 1);
     assert!(board.number_to_coordinates[&1] == (4, 0));
-
-    assert!(board.board[0][4] == 0);
     assert!(board.number_to_coordinates[&0] == (0, 4));
 
     assert!(board.drawn_numbers.is_empty());
@@ -239,4 +226,32 @@ fn part_1_given_example() {
 }
 
 #[test]
-fn part_2_given_example() {}
+fn part_2_given_example() {
+    // GIVEN
+    let input = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+    22 13 17 11  0
+     8  2 23  4 24
+    21  9 14 16  7
+     6 10  3 18  5
+     1 12 20 15 19
+    
+     3 15  0  2 22
+     9 18 13 17  5
+    19  8  7 25 23
+    20 11 10 24  4
+    14 21 16 12  6
+    
+    14 21 17 24  4
+    10 16 15  9 19
+    18  8 23 26 20
+    22 11 13  6  5
+     2  0 12  3  7";
+
+    // WHEN
+    let bingo_game = parse_input(input.split("\n").collect());
+    let r = compute_part2(&bingo_game);
+
+    // THEN
+    assert!(r == 1924)
+}
